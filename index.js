f250bee51f6e0aa9252ee52671e3f2f272a06448
RomaSamkov <Romansamkov.rs@gmail.com>
2025-01-07 19:01:14 +0200

    add create index.js and add class

---
+++ index.js
@@ -0,0 +1,30 @@
+// && возвращает первое ложное значение (или последнее истинное, если все истинны).
+// || возвращает первое истинное значение (или последнее ложное, если все ложные).
+
+class Person {
+  #email;
+  constructor(name, age, email) {
+    this.name = name;
+    this.age = age;
+    this.#email = email;
+  }
+
+  get getmail() {
+    return this.#email;
+  }
+
+  set setEmail(newEmail) {
+    return (this.#email = newEmail);
+  }
+
+  setShowAge() {
+    console.log(this.age);
+  }
+}
+
+const kevin = new Person("Kevin", 25, "kevin@mail.com");
+console.log(kevin.getmail);
+kevin.setEmail = "It";
+console.log(kevin.getmail);
+
+kevin.setShowAge();
